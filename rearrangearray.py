6b4d54d8b3ce4e26ce295a89a51b5bf9612ef6da
FaijWaris <faijwaris692@gmail.com>
2026-01-20 22:10:10 +0530

    fix

--- rearrangearray.py
+++ rearrangearray.py
@@ -1,15 +1,19 @@
-nums=[5,10,-3,-1,-10,6]
-n=len(nums)
-result=[0]*n
-posIndex,negIndex=0,1
-def rearangearray(n,result,posIndex,negIndex):
- for i in range(0,n):
-   if nums[i]>=0:
-     result[posIndex]=nums[i]
-     posIndex+=2 
-   else:
-     result[negIndex]=nums[i]
-     negIndex+=2
-   return result  
-    
-print(rearangearray(n,result,posIndex,negIndex))    
+def rearrange_array(nums):
+    n = len(nums)
+    result = [0] * n
+    pos_index = 0
+    neg_index = 1
+
+    for num in nums:
+        if num >= 0 and pos_index < n:
+            result[pos_index] = num
+            pos_index += 2
+        elif num < 0 and neg_index < n:
+            result[neg_index] = num
+            neg_index += 2
+
+    return result
+
+
+nums = [5, 10, -3, -1, -10, 6]
+print(rearrange_array(nums))
