c3b272f713ae5eea3b136e8d08b827b6a6b77827
FaijWaris <faijwaris692@gmail.com>
2026-01-21 23:08:47 +0530

    new fix

--- quicksort.py
+++ quicksort.py
@@ -1,28 +1,26 @@
-nums = [3, 6, 8, 10, 1, 2, 1]
-
-def partition(nums, low, high):
-    pivot = nums[low]
-    i = low
-    j = high
-
-    while i < j:
-        while i <= high - 1 and nums[i] <= pivot:
-            i += 1
-        while j >= low + 1 and nums[j] > pivot:
-            j -= 1
-        if i < j:
-            nums[i], nums[j] = nums[j], nums[i]
-
-    nums[low], nums[j] = nums[j], nums[low]
-    return j
-
-
 def quick_sort(nums, low, high):
-    if low < high:
-        pindex = partition(nums, low, high)
-        quick_sort(nums, low, pindex - 1)
-        quick_sort(nums, pindex + 1, high)
+    if low >= high:
+        return
+
+    pivot = nums[(low + high) // 2]
+    i, j = low, high
+
+    while i <= j:
+        while nums[i] < pivot:
+            i += 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i <= j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+
+    quick_sort(nums, low, j)
+    quick_sort(nums, i, high)
 
 
+nums = [3, 6, 8, 10, 1, 2, 1]
 quick_sort(nums, 0, len(nums) - 1)
 print(nums)
+
