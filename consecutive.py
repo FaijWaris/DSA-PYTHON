51d3c2356a934c62344b319165af2ecae8a16f71
FaijWaris <faijwaris692@gmail.com>
2026-01-23 23:16:26 +0530

    done

--- consecutive.py
+++ consecutive.py
@@ -1,16 +1,16 @@
-nums = [1,1,2,3,1,1,1,1,4,5,1,1]
-
 def maxones(nums):
-    count = 0
-    max_count = 0
-    
-    for i in range(len(nums)):
-        if nums[i] == 1:
+    count = max_count = 0
+
+    for num in nums:
+        if num == 1:
             count += 1
-            max_count = max(max_count, count)  # update here
+            if count > max_count:
+                max_count = count
         else:
             count = 0
-    
-    return max_count   # return AFTER loop
 
+    return max_count
+
+nums = [1,1,2,3,1,1,1,1,4,5,1,1]
 print(maxones(nums))
+
