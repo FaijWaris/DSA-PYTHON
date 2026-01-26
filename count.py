fdc96aa55d7f0892e970c4704b0bd8f1fd0f7a17
FaijWaris <faijwaris692@gmail.com>
2026-01-26 20:07:29 +0530

    done fix

--- count.py
+++ count.py
@@ -1,11 +1,3 @@
-n=5873
-num=n
-count=0
-while num >0:
-    count+=1
-    num=num//10
+n = 5873
+count = len(str(abs(n)))
 print("Number of digits:", count)
-#same count doing with log logic
-# from math import *
-# def countdigits(num):
-#     return int(log10(num))+1
