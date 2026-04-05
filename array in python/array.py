9392e87fe2a85a0ae0b552766d57d49f9e890a3d
FaijWaris <faijwaris692@gmail.com>
2026-04-06 00:13:13 +0530

    add initial implementation of array and numpy examples

---
+++ array in python/array.py
@@ -0,0 +1,32 @@
+# import array
+from array import * # best aprach
+# val =array('i',[1,2,3,4,5,6])
+# for i in range (0,6):
+#     print(val[i], end=' ' )
+
+# print("\n")
+
+# for x in val: #enhance for loop
+#     print(x ,end= ' , ')
+
+# #insert ,append isse update kr skte hai
+# #how to create a list from array
+# copyarray=array(val.typecode ,(x for x in val))
+# print(copyarray)    
+#array me user se no. dalwa rhe hai
+# arr= array('i'  ,[])
+# n=int(input('enter a no.'))
+
+# for i in range (0,n):
+#     arr.append(int(input('enter next input')))
+
+# for x in arr:
+#     print(x ,end=' ')    
+
+
+#searching element in array
+arr=array( 'i' ,[12,3,4,5,2,3,4])
+
+i=arr.index(5)
+
+print(i)
---
+++ array in python/numpy.py
@@ -0,0 +1,5 @@
+from numpy import *
+#linespace
+val=linspace(10,20,5)
+for x in val :
+    print(x ,end=" ")
